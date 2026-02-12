1454aa7d731d777e25e8831c63ed414023d81544
aanand kumawat <aanandkumawat@zohomail.in>
2026-02-12 19:11:07 +0530

    want to show how to fech remote updates without mergin

--- src/bitManipulation/gitPrectice.java
+++ src/bitManipulation/gitPrectice.java
@@ -2,6 +2,6 @@ package bitManipulation;
 
 public class gitPrectice {
     public static void main(String[] args) {
-
+        System.out.println("hello this changes for only git prectice and also i want to see how to fetch the remote changes in our local ");
     }
 }
